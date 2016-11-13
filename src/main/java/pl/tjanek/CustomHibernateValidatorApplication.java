b622d25bc289372048a38ad67e5790318b9061d8
Tomasz Janek <janek.tomasz@gmail.com>
2016-11-13 10:28:39 +0100

    localdate serialization for jackson

--- src/main/java/pl/tjanek/CustomHibernateValidatorApplication.java
+++ src/main/java/pl/tjanek/CustomHibernateValidatorApplication.java
@@ -1,7 +1,31 @@
 package pl.tjanek;
 
-/**
- * Created by Tomek on 13.11.2016.
- */
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
 public class CustomHibernateValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomHibernateValidatorApplication.class, args);
+    }
+
+    @Bean
+    public ObjectMapper serializingObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDate.class, LocalDateSerializer.INSTANCE);
+        javaTimeModule.addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE);
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
+    }
+
 }
